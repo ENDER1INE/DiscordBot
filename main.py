5bae253aeaa92fa5b4717353a73830fa8cbaba73
SofiaBerezina <sofiaber114@gmail.com>
2022-03-29 23:53:44 +0500

    2

--- main.py
+++ main.py
@@ -11,5 +11,15 @@ async def hello(ctx):
     author = ctx.message.author
     await ctx.send(f"Привет {author.mention}")
 
+@bot.command()
+async def help(ctx):
+    commands = [f'Вызов бота = "+"',
+                f'⭕ hello: - команда приветстия',
+                f'⭕ join: - подключение бота к голосовому чату',
+                f'⭕ leave: - отключение бота от голосового чата']
+
+    embed = discord.Embed(color=0xff9900, title=f'Вызов бота = ">"')
+    embed.add_field(name='<<commands>>', value='\n'.join(commands), inline=True)
+    await ctx.send(embed=embed)
 
 bot.run(settings['token'])
