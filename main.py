1270f703a3de7f8319849496cea048e44835960d
Ender1ine <p2p.two@mail.ru>
2022-04-02 22:02:57 +0300

    wather_options

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from config import settings
+import requests
 
 
 bot = commands.Bot(command_prefix=settings['prefix'], help_command=None)
@@ -10,6 +11,23 @@ bot = commands.Bot(command_prefix=settings['prefix'], help_command=None)
 async def hello(ctx):
     author = ctx.message.author
     await ctx.send(f"Привет {author.mention}")
+
+
+@bot.command()
+async def weather(ctx, city):
+    key = "4404863c7b41019dcdccb79c8d750ac8"
+    response = requests.get(f"http://api.openweathermap.org/data/2.5/find?q={city}&type=like"
+                            f"&APPID={key}").json()
+    response = response["list"][0]
+
+    wind = response["wind"]
+    rain = response["rain"]
+    clouds = response["clouds"]
+    snow = response["snow"]
+    temperature = int(response["main"]["temp"]) - 273
+    print(temperature)
+    print(response)
+    await ctx.send(f"Температура в {response['name']} сейчас {temperature}")
 
 
 bot.run(settings['token'])
