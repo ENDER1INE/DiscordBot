5e3495efab6609d82ad4a3ec69308391cde486ac
Ender1ine <p2p.two@mail.ru>
2022-04-03 18:04:05 +0300

    wather_options_1

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 from config import settings
 import requests
-
+import json
 
 bot = commands.Bot(command_prefix=settings['prefix'], help_command=None)
 
@@ -16,18 +16,36 @@ async def hello(ctx):
 @bot.command()
 async def weather(ctx, city):
     key = "4404863c7b41019dcdccb79c8d750ac8"
-    response = requests.get(f"http://api.openweathermap.org/data/2.5/find?q={city}&type=like"
-                            f"&APPID={key}").json()
-    response = response["list"][0]
+    url = f"http://api.openweathermap.org/data/2.5/find?q={city}&type=like&APPID={key}"
+    response = requests.get(url).json()
 
-    wind = response["wind"]
-    rain = response["rain"]
-    clouds = response["clouds"]
-    snow = response["snow"]
-    temperature = int(response["main"]["temp"]) - 273
-    print(temperature)
-    print(response)
-    await ctx.send(f"Температура в {response['name']} сейчас {temperature}")
+    response = response["list"][0]
+    weather = \
+        {
+            "Скорость ветра": f"{response['wind']['speed']} м/с",
+            "Дождь": response["rain"],
+            "Облачность": response["clouds"],
+            "Снег": response["snow"],
+            "Минимальная температура": int(response["main"]["temp_min"]) - 273,
+            "Максимальная температура": int(response["main"]["temp_max"]) - 273
+        }
+
+    color = 0xFF6500
+    embed = discord.Embed(
+        title=f"Погода в городе {city}",
+        color=color
+    )
+    print(weather)
+    for key in weather:
+        print(key)
+        embed.add_field(
+            name=key,
+            value=str(weather[key]),
+            inline=False
+        )
+
+    print(123)
+    await ctx.send(embed=embed)
 
 
 bot.run(settings['token'])
