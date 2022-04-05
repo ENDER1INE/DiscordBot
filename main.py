6470c74f3827a32b9a7722fc6b96916f97c41b46
Ender1ine <p2p.two@mail.ru>
2022-04-05 19:23:30 +0300

    wather_options_max_finally_version

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 from config import settings
 import requests
-import json
+
 
 bot = commands.Bot(command_prefix=settings['prefix'], help_command=None)
 
@@ -20,6 +20,14 @@ async def weather(ctx, *city):
         key = "4404863c7b41019dcdccb79c8d750ac8"
         url = f"http://api.openweathermap.org/data/2.5/find?q={city}&type=like&APPID={key}"
         response = requests.get(url).json()["list"][0]
+        country = response["sys"]["country"]
+        if country == "RU":
+            geocoder_response = requests.get(f"https://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-"
+                                             f"98ba-98533de7710b&geocode={response['coord']['lon']},"
+                                             f"{response['coord']['lat']}&format=json").json()
+            geocoder_response = geocoder_response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"] \
+                ["metaDataProperty"]["GeocoderMetaData"]["Address"]["Components"][2]["name"]
+            country = f"{country}, {geocoder_response}"
         rain = "No rain observed " if not response["rain"] else "Chance of rain"
         snow = "No snowfall observed " if not response["snow"] else "Snowfall possible "
         weather = \
@@ -32,7 +40,7 @@ async def weather(ctx, *city):
             }
         color = 0x00FFFF
         embed = discord.Embed(
-            title=f"Погода в городе {city}",
+            title=f"Погода в городе {city.capitalize()}({country})",
             description=f"{response['weather'][0]['description']}",
             color=color
         )
