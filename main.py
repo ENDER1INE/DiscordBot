de6b183c3cca2b8b94a74cbe0bfebfea1b20e5bd
SofiaBerezina <sofiaber114@gmail.com>
2022-04-26 18:22:27 +0500

    новости2

--- main.py
+++ main.py
@@ -29,6 +29,8 @@ def update(user_id, key_name, value):
     return users
 
 
+
+
 @bot.command()
 async def hello(ctx):
     author = ctx.message.author
@@ -206,23 +208,30 @@ async def news(ctx, country):
                f'apiKey=973085d982b24f93a43101b3fba958bd')
         responce = requests.get(url)
         json_data = json.loads(responce.text)
-        number = random.randint(1, len(json_data['articles']))
+        number = random.randint(0, len(json_data['articles']) - 1)
         try:
             if json_data['articles'][number]['title'] in users.get(author.id).get('news'):
                 await ctx.send(
                     embed=discord.Embed(color=0xFF2B2B, title=f"Кажется эту новость вы уже слышали, сейчас подберем другую"))
-                number = random.randint(1, len(json_data['articles']))
+                if len(users.get(author.id).get('news')) == len(json_data['articles']):
+                    raise MemoryError(
+                        await ctx.send(
+                            embed=discord.Embed(color=0xFF2B2B,
+                                                title=f"Кажется новости на сегодня закончились")))
+                else:
+                    while json_data['articles'][number]['title'] in users.get(author.id).get('news'):
+                        number = random.randint(0, len(json_data['articles']) - 1)
             else:
                 update(author.id, 'news', json_data['articles'][number]['title'])
         except Exception:
             update(author.id, 'news', json_data['articles'][number]['title'])
-        print(json_data['articles'][number])
         embed_2 = discord.Embed(color=0xff9900, title=json_data['articles'][number]['title'])
         embed_3 = discord.Embed(color=0xff9900, title=json_data['articles'][number]['url'])
         embed = discord.Embed(color=0xff9900)
         embed_2.add_field(name='<<news>>', value=json_data['articles'][number]['description'], inline=True)
         embed.set_image(url=json_data['articles'][number]['urlToImage'])
-        await ctx.send(embed=embed)
+        if json_data['articles'][number]['urlToImage']:
+                await ctx.send(embed=embed)
         await ctx.send(embed=embed_2)
         await ctx.send(embed=embed_3)
     except Exception:
