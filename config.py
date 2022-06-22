209d8c5a8748947968ae23d5c8d73ba6082446f5
ENDER1INE <94693544+ENDER1INE@users.noreply.github.com>
2022-06-22 18:07:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 settings = {
-    'token': 'OTU4Mzg5MjU4OTc1MDU1ODg0.YkMnjQ.erMR1n3F-WfsLhtKfbEnGa4SD0M',
+    'token': '',
     'bot': 'Rodnik Prokamya',
     'id': 958389258975055884,
     'prefix': '>'
